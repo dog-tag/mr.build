4160cc3ba125bad60b7765b3dc333d906c1cdf71
DogTag <dogtag@protonmail.com>
2017-02-24 08:41:26 +0530

    updating buildme

--- build_configs/__init__.py
+++ build_configs/__init__.py
@@ -5,7 +5,7 @@ PY_VERSION = '2.7.13'
 
 from collections import namedtuple
 
-def build_command(cmd,
+def build_expr(cmd,
                   setup=None,
                   teardown=None,
                   mode='fg',
@@ -29,12 +29,12 @@ def build_command(cmd,
     setup = setup or list()
     teardown = teardown or list()
 
-    Runnable = namedtuple(
-        'Runnable',
+    BuildExpr = namedtuple(
+        'BuildExpr',
         'cmd, setup, teardown, mode, log, desc, on_shell, on_success, on_error'
     )
 
-    return Runnable(
+    return BuildExpr(
         cmd=cmd,
         setup=setup,
         teardown=teardown,
@@ -47,43 +47,70 @@ def build_command(cmd,
     )
 
 
+SimpleCommand = namedtuple('SimpleCommand', 'cmd')
+ComplexCommand = namedtuple('ComplexCommand', 'if_, if_axn, else_axn')
+
+def simple_command(cmd):
+    return SimpleCommand(cmd=cmd.split())
+
+def complex_command(if_=None, if_axn=None, else_axn=None):
+
+    if_ = if_ and if_.split()
+    if_axn = if_axn.split() if isinstance(if_axn, str) else if_axn
+    else_axn = else_axn.split() if isinstance(else_axn, str) else else_axn
+
+    return ComplexCommand(if_=if_, if_axn=if_axn, else_axn=else_axn)
+
+
 class ChangeDir(object):
-    def __init__(self, target):
+    def __init__(self, target=None):
         self.target = target
+
+
+class TextMessage(object):
+    def __init__(self, msg):
+        self.message = msg
+
+
+class AbortBuild(object):
+    def __init__(self, message):
+        self.message = message
 
 
 BUILD_COMMANDS = [
 
-    build_command(
-        cmd=('ls', '-la', ),
-        desc='Listing all the files and folders...',
-        on_success='Command %(cmd)s ran successfully !',
-        on_error='Command %(cmd)s failed !. ERROR: {}',
-    ),
-
     #
     # Install Python as Virtual Environment.
-    build_command(
-        cmd=('ls'),  #'wget', '-P', 'downloads/', 'https://www.python.org/ftp/python/{0}/Python-{0}.tar.xz'.format(PY_VERSION), ),
-        setup=[
-            ('cd', 'downloads/', )
-        ],
-        teardown=[
+    build_expr(
+
+        desc='Downloading and installing Custom Python version'.format(PY_VERSION),
+
+        cmd=[
+            complex_command(
+                if_='find usr/bin/ -name python',
+                if_axn=AbortBuild('Python is already installed !, please remove the  existing one.'),
+                else_axn=TextMessage('New Python will be installed !')
+            ),
+            complex_command(
+                if_='find downloads/ -name Python-{0}.tar.xz'.format(PY_VERSION),
+                if_axn=TextMessage('Download Exists -- Python-{0}.tar.xz'.format(PY_VERSION)),
+                else_axn='wget -P downloads/ https://www.python.org/ftp/python/{0}/Python-{0}.tar.xz'.format(PY_VERSION)
+            ),
             ChangeDir('downloads/'),
-            ('tar', '-xvJf', 'Python-{0}.tar.xz'.format(PY_VERSION)),
+            simple_command('tar -xvJf Python-{0}.tar.xz'.format(PY_VERSION)),
             ChangeDir('downloads/Python-{0}/'.format(PY_VERSION)),
-            ('./configure', '--prefix={0}/usr/'.format(os.getcwd()), '--enable-unicode=ucs4'),
-            ChangeDir('downloads/Python-{0}/'.format(PY_VERSION)),
-            ('make'),
-            ChangeDir('downloads/Python-{0}/'.format(PY_VERSION)),
-            ('make', 'install'),
-            ChangeDir('../'),
-            ('rm', '-rvf', 'downloads/Python-{0}/'.format(PY_VERSION), ),
-            ChangeDir('../'),
+            simple_command('./configure --prefix={0}/usr/ --enable-unicode=ucs4'.format(os.getcwd())),
+            simple_command('make'),
+            simple_command('make install'),
         ],
-        desc='Fetching Python',
+
+        teardown=[
+            ChangeDir(),
+            simple_command('rm -rvf downloads/Python-{0}/'.format(PY_VERSION)),
+        ],
+
         on_success='Command %(cmd)s ran successfully !',
-        on_error='Command %(cmd)s failed !. ERROR: {}',
+        on_error='Failed while running the command %(cmd)s. ERROR: {}',
     ),
 
 ]
--- buildme.py
+++ buildme.py
@@ -6,53 +6,72 @@ import traceback
 
 from subprocess import call, Popen, check_output
 
-from build_configs import BUILD_COMMANDS, ChangeDir
+from build_configs import BUILD_COMMANDS, ChangeDir, ComplexCommand, SimpleCommand, TextMessage, AbortBuild
 
-print BUILD_COMMANDS
+from pprint import pprint as pp
+
+pp(BUILD_COMMANDS)
 
 BUILD_DIR = os.getcwd()
 
+
+def execute(cmd):
+    import pdb; pdb.set_trace() ## XXX: Remove This
+    if isinstance(cmd, AbortBuild):
+        raise Exception(cmd.message)
+    return check_output(cmd)
+
+def test_execute(cmd):
+    return bool(check_output(cmd))
+
+
+def evaluate(command_list):
+
+    for command in command_list:
+        print ">>>", command
+        if isinstance(command, ChangeDir):
+            if command.target:
+                change_to = os.path.join(BUILD_DIR, command.target)
+            else:
+                change_to = BUILD_DIR
+
+            os.chdir(change_to)
+
+        if isinstance(command, SimpleCommand):
+            import pdb; pdb.set_trace() ## XXX: Remove This
+            print execute(command.cmd)
+
+        if isinstance(command, ComplexCommand):
+
+            if command.if_ and test_execute(command.if_):
+                if command.if_axn:
+                    if isinstance(command.if_axn, TextMessage):
+                        print command.if_axn.message
+                    else:
+                        print execute(command.if_axn)
+            elif command.else_axn:
+                if isinstance(command.else_axn, TextMessage):
+                    print command.else_axn
+                else:
+                    print execute(command.else_axn)
+
+
 for construct in BUILD_COMMANDS:
 
-    for pre_set in construct.setup:
-        print "$ ", ' '.join(pre_set)
-        check_output(pre_set)
-
-    print construct.desc
+    import pdb; pdb.set_trace() ## XXX: Remove This
     try:
-        out = check_output(construct.cmd)
+        if construct.setup:
+            pass
 
-        if construct.on_shell and out:
-            print out
+        if construct.setup:
+            evaluate(construct.setup)
+
+        if construct.cmd:
+            evaluate(construct.cmd)
+
+        if construct.teardown:
+            evaluate(construct.teardown)
 
     except Exception as error:
+        print str(error)
 
-        _err_tpl = construct.on_error % construct._asdict()
-
-        print _err_tpl.format(str(error))
-        print traceback.format_exc()
-
-        # Break me building
-        break
-
-    else:
-        print construct.on_success % construct._asdict()
-
-    for reset in construct.teardown:
-
-        #import pdb; pdb.set_trace() ## XXX: Remove This
-        if isinstance(reset, ChangeDir):
-            
-            print ">>>", os.getcwd()
-            #import pdb; pdb.set_trace()
-            os.chdir(os.path.join(BUILD_DIR, reset.target))
-            print reset.target
-            print ">>>", os.getcwd()
-        #elif not isinstance(reset, ChangeDir):
-        #    os.chdir(BUILD_DIR)
-        else:
-            print '=========================================\n', reset
-            cmd = Popen(reset)
-            out = cmd.communicate()
-
-            print out
